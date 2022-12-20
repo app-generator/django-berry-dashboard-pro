197c009205f3d08b5845e6103daf59dd27b20e61
mominur-helios <mominur.rahman@heliosinsider.com>
2022-12-20 12:13:24 +0600

    fix deploy issue

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
