61949713774f96572d666dd9b3fea855a70da66d
App Generator <51070104+app-generator@users.noreply.github.com>
2022-12-21 08:13:42 +0200

    Settings - Disable Whitenoise Compression

--- core/settings.py
+++ core/settings.py
@@ -148,8 +148,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#if not DEBUG:
+#    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
